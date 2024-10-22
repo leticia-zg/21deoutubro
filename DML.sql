2412fa4aa4856058ae902ef03cae30ba7fdc173d
labsfiap <labsfiap@PA501MICRO20.fiap.com.br>
2024-10-21 22:22:52 -0300

    n

---
+++ DML.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_endereco (
+    cod_endereco,
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'AV PAULISTA',
+    1024,
+    '01308000',
+    'Bela Vista',
+    'São Paulo',
+    'Brasil',
+    'FIAP'
+);
